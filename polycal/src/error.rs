5b6e9db3545a6b6e01405757ac9ef34adb2d73e9
cgubbin <chris.gubbin@gmail.com>
2024-12-12 12:51:11 +0000

    When a polynomial is created in `polycal`, it is done using a finite range of independent values, and domain of dependent values. This is the only data for which the fit can be assumed to be accurate: no data was provided for predictions to be made outside this range.
    The previous version of `polycal` returned an error in the forward
    problem if the argument was outside the range of independent values used
    to create the polynomial, and one in the backward direction if the
    argument was outside the range of dependent values used to create the
    polynomial. No information was attached to the error, other than the
    fact the input was out of bounds.
    
    In some applications like numerical optimisation it is useful to query
    the objective function continuously across the parameter space. Not
    being able to do this prevented users from safely using, for example,
    Gauss Newton optimisation.
    
    To resolve this new error variants were added to describe the case where
    the input is out-of-bounds. An error is still returned
    (`OutOfBoundsCertain`, `OutOfBoundsUncertain`), but the error now
    contains the value requested.
    
    This means that users can choose to access and use the result of their
    request if necessary.
    
    Note that monotonicity is only checked on the *original* range of the
    function. It is entirely possible roots may be repeated outside this
    range. If the polynomial is queried out of range, then the caller should
    always verify the result of the optimisation is within the original
    range of the function.

--- polycal/src/error.rs
+++ polycal/src/error.rs
@@ -14,6 +14,17 @@ pub enum Kind {
 pub enum PolyCalError<E> {
     /// The input value was outside of the calibration range, so a prediction cannot be reliably
     /// made.
+    ///
+    /// In this scenario we return the value anyway because the caller needs to decide what to do.
+    /// In some scenarios using the value might be acceptable, particularly when the input value
+    /// was very close to the calibration range. Alternatively if the caller is running an
+    /// unconstrained optimisation the solver could leave the finite function domain. In that case,
+    /// as long as the value solved for lies in the function domain at the end of the optimisation
+    /// there is no harm in leaving it during the solve.
+    ///
+    /// Note though that guarantees around monotonicity are only made within the domain of the
+    /// polynomial. It is possible a root outside the domain may be found, and it is the
+    /// responsibility of the caller to check.
     #[error("input {kind:?} ({value}) is out of {kind:?} calibration working range: {range:?}")]
     OutOfRangeCertain {
         value: E,
@@ -21,6 +32,19 @@ pub enum PolyCalError<E> {
         range: Range<E>,
         kind: Kind,
     },
+    /// The input value was outside of the calibration range, so a prediction cannot be reliably
+    /// made.
+    ///
+    /// In this scenario we return the value anyway because the caller needs to decide what to do.
+    /// In some scenarios using the value might be acceptable, particularly when the input value
+    /// was very close to the calibration range. Alternatively if the caller is running an
+    /// unconstrained optimisation the solver could leave the finite function domain. In that case,
+    /// as long as the value solved for lies in the function domain at the end of the optimisation
+    /// there is no harm in leaving it during the solve.
+    ///
+    /// Note though that guarantees around monotonicity are only made within the domain of the
+    /// polynomial. It is possible a root outside the domain may be found, and it is the
+    /// responsibility of the caller to check.
     #[error("input {kind:?} ({value}) is out of {kind:?} calibration working range: {range:?}")]
     OutOfRangeUncertain {
         value: E,
